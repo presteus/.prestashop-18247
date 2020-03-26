438d3251191c17840bf70730bd903ab6bec7050c
Presteus Sparkling <presteus.sparkling@gmail.com>
2020-03-26 12:19:21 +0100

    Simplify

--- php/scripts/docker-run.sh
+++ php/scripts/docker-run.sh
@@ -3,19 +3,6 @@
 # From now, stop at error
 set -euxo pipefail
 
-git config --global user.name developer
-git config --global user.email developer@prestashop.local
-
-# Uncomment line below to ignore case-sensitive
-#git config --global core.ignorecase true
-
-if ! [ -f /usr/local/bin/composer ]; then
-  printf '* Download and install Composer...\n'
-  php -r "copy('https://getcomposer.org/installer', '/tmp/composer-setup.php');"
-  php /tmp/composer-setup.php --no-ansi --install-dir=/usr/local/bin --filename=composer
-  rm -rf /tmp/composer-setup.php
-fi
-
 cd /var/www/html
 
 if [ ! -f index.php ]; then
